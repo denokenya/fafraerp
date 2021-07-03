08b0358aa349a71530d04dc564397f321fc16e90
denokenya <dennismuteti25@gmail.com>
2021-07-03 13:07:46 +0300

    updated roles,department and designation api

--- backend/backend/employee/urls.py
+++ backend/backend/employee/urls.py
@@ -1,7 +1,15 @@
 from . import views
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register("api", views.EmployeeViewSet, "employees")
+router.register("designationapi", views.DesignationViewSet, "designations")
+router.register("departmentapi", views.DepartmentViewSet, "departments")
+router.register("roleapi", views.RoleViewSet, "roles")
+router.register("employeeapi", views.EmployeeViewSet, "employees")
 
-urlpatterns = router.urls
+
+#urlpatterns = router.urls
+urlpatterns = [
+    path('', include(router.urls)),
+]
