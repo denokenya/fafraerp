1093efc9e3dc11b69fe6f3589bcc6aba0a6c8b64
denokenya <dennismuteti25@gmail.com>
2021-07-03 12:41:57 +0300

    code refactoring

--- backend/backend/employee/views.py
+++ backend/backend/employee/views.py
@@ -1,7 +1,25 @@
 from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import ModelViewSet
-from .models import Employee
-from .serializers import EmployeeSerializer
+from .models import Employee, Designation, Department, Role
+from .serializers import EmployeeSerializer, DepartmentSerializer, DesignationSerializer, RoleSerializer
+
+
+class DesignationViewSet(ModelViewSet):
+    queryset = Designation.objects.all()
+    serializer_class = DesignationSerializer
+    permission_classes = [AllowAny]
+
+
+class DepartmentViewSet(ModelViewSet):
+    queryset = Department.objects.all()
+    serializer_class = DepartmentSerializer
+    permission_classes = [AllowAny]
+
+
+class RoleViewSet(ModelViewSet):
+    queryset = Role.objects.all()
+    serializer_class = RoleSerializer
+    permission_classes = [AllowAny]
 
 
 class EmployeeViewSet(ModelViewSet):
