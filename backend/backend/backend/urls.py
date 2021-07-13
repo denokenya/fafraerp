7eb81ee5ee155592f01880bdb49a292b5e291760
denokenya <dennismuteti25@gmail.com>
2021-07-13 14:04:58 +0300

    setting jwt urls

--- backend/backend/backend/urls.py
+++ backend/backend/backend/urls.py
@@ -18,11 +18,16 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.schemas import get_schema_view
 from rest_framework.documentation import include_docs_urls
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('employee.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('accounts/', include('rest_framework.urls')),
     path('schema/', get_schema_view(
         title="SchoolAPI",
